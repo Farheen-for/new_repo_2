cf87dc08f19dacb83e7f9673426b0df0fe21f499
Farheen <farheen123@gmail.com>
2026-02-20 10:30:22 +0530

    Pused

--- CRM_Framework/src/test/java/organization_module/Class_2.java
+++ CRM_Framework/src/test/java/organization_module/Class_2.java
@@ -7,5 +7,7 @@ public class Class_2 {
 	      @Test
 	      public void createClass() {
 	    	  System.out.println("class_2 created");
+	    	  System.out.println("class_67 created");
+	    	  System.out.println("class_2 created");
 	      }
 }
