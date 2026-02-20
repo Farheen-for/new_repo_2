c6e6271df3d6220abc53780eaf65d2fec0c24f99
Farheen <farheen123@gmail.com>
2026-02-20 10:29:04 +0530

    added new class

---
+++ CRM_Framework/src/test/java/organization_module/Class_2.java
@@ -0,0 +1,11 @@
+package organization_module;
+
+import org.testng.annotations.Test;
+
+public class Class_2 {
+	
+	      @Test
+	      public void createClass() {
+	    	  System.out.println("class_2 created");
+	      }
+}
